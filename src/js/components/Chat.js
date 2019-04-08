dd78c786ce62188f284592ce68c3ec562d73963f
Alexander Nikolskiy <alexxxnikolskiy@gmail.com>
2019-04-08 23:01:17 +0300

    Edit Chat component

--- src/js/components/Chat.js
+++ src/js/components/Chat.js
@@ -1,345 +1,134 @@
 import pubsub from 'Utils/pubsub';
 import io from 'Utils/io';
-import { getRoomMessages, getPrivateMessages } from 'Utils/helpers';
-import Message from './message/index';
-import MessageOutput from './message/output/messageOutput';
-import MessageRepeatedOutput from './message/output/messageRepeatedOutput';
-import MessageEditOutput from './message/output/messageEditOutput';
-import MessageNewOutput from './message/output/messageNewOutput';
+import { getRoomMessages, getPrivateMessages } from 'Utils/db';
 
 class Chat {
   constructor() {
-    this.elem = document.querySelector('.chat-box__body-list');
-    this.messages = [];
     this.sender = null;
     this.receiver = null;
     this.lastVisit = Date.now();
-    this.setHandlers();
+    this.components = new Set();
 
-    io.on('room:join', this.ioJoinRoom.bind(this));
-    io.on('room:leave', this.ioLeaveRoom.bind(this));
-    io.on('message:add', this.ioAddMessage.bind(this));
     io.on('login', this.ioLogin.bind(this));
-    io.on('message:edit', this.ioEditMessage.bind(this));
-    io.on('message:delete', this.ioDeleteMessage.bind(this));
-    io.on('member:join', this.loadMessages.bind(this));
+    io.on('message:add', this.ioAddMessage.bind(this));
+    io.on('member:join', this.ioMemberJoin.bind(this));
 
-    pubsub.sub('room:join', this.loadMessages.bind(this));
-    pubsub.sub('room:select', this.selectRoom.bind(this));
-    pubsub.sub('editor:new-line', this.scroll.bind(this));
-    pubsub.sub('message:edit', this.editMessage.bind(this));
-    pubsub.sub('message:cancel', this.cancelEditMessage.bind(this));
-    pubsub.sub('message:save', this.saveMessage.bind(this));
-    pubsub.sub('message:edit-last', this.editLastMessage.bind(this));
-    pubsub.sub('message:delete', this.deleteMessage.bind(this));
+    pubsub.sub('room:select', this.handleSelectRoom.bind(this));
+    pubsub.sub('room:join', this.handleJoinRoom.bind(this));
+    pubsub.sub('message:write', this.handleWriteMessage.bind(this));
     pubsub.sub('member:select', this.selectMember.bind(this));
-    pubsub.sub('message:add', this.addMessage.bind(this));
   }
 
-  setHandlers() {
-    this.elem.addEventListener('mouseover', ev => {
-      const messageElem = ev.target.closest('.message');
-
-      if (!messageElem || messageElem.classList.contains('message_editing')) {
-        return;
-      }
-
-      const messageId = messageElem.dataset.id;
-      const actionsElem = messageElem.getElementsByClassName('message__action-list')[0];
-
-      if (!actionsElem && messageId) {
-        const message = this.messages.find(msg => msg.id === messageId);
-        const isOwnMessage = message.sender === this.sender.username;
-
-        messageElem.append(message.renderActions({ isOwnMessage, isAutomated: message.automated }));
-      }
-    });
-
-    this.elem.addEventListener('mouseout', ev => {
-      const messageElem = ev.target.closest('.message');
-
-      if (!messageElem || messageElem.classList.contains('message_editing') || !ev.relatedTarget) {
-        return;
-      }
-
-      const where = ev.relatedTarget.closest('.message__action-list');
-      const elems = this.elem.querySelectorAll('.message__action-list');
-
-      elems.forEach(elem => {
-        if (elem !== where) {
-          elem.parentElement.removeChild(elem);
-        }
-      });
-    });
-
-    const observer = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        if (mutation.addedNodes.length > 1) {
-          return;
-        }
-
-        const elem = mutation.addedNodes[0];
-        if (!elem || !elem.classList.contains('message_editing')) {
-          return;
-        }
-
-        const input = elem.querySelector('.message__editor-input');
-        input.setAttribute('style', `height:${input.scrollHeight}px;`);
-        input.focus();
-        setTimeout(() => {
-          input.selectionStart = input.value.length;
-          input.selectionEnd = input.value.length;
-        }, 0);
-      });
-    });
-
-    observer.observe(this.elem, {
-      childList: true,
-    });
-  }
-
-  ioLogin(user) {
+  async ioLogin(user) {
     this.sender = user;
   }
 
-  ioJoinRoom(user) {
-    const message = new Message({
-      id: user.message,
-      sender: user.username,
-      text: `joined #${user.room.name}`,
-      automated: true,
-    });
+  isNotEligibleMessageForDisplay(message) {
+    const isPrivateMessage = !!message.receiver;
 
-    this.messages.push(message);
-    this.render();
+    return (
+      ((!this.receiver && isPrivateMessage) || // if private message, but we in the room
+      (this.receiver && !isPrivateMessage) || // if not private message, but we are writing a private message
+        (this.receiver && // if private message from one user, but we are chatting with another one (not with yourself)
+          this.receiver.username !== message.sender &&
+          this.sender.username !== this.receiver.username)) &&
+      this.sender.username !== message.sender
+    );
   }
 
-  ioLeaveRoom(user) {
-    const message = new Message({
-      id: user.message,
-      sender: user.username,
-      text: `left #${user.room.name}`,
-      automated: true,
-    });
+  shouldShowNewPrivateMessageNotification(message) {
+    const isPrivateMessage = !!message.receiver;
 
-    this.messages.push(message);
-    this.render();
-  }
-
-  ioAddMessage(message, isPrivate) {
-    if (this.sender.room === message.room) {
-      if (!this.receiver && isPrivate) {
-        pubsub.pub('message:new-private', message.sender);
-      } else if (this.receiver && !isPrivate) {
-        pubsub.pub('message:new', message.room);
-      } else if (
-        this.receiver &&
-        isPrivate &&
+    return (
+      (!this.receiver && isPrivateMessage) ||
+      (this.receiver &&
+        isPrivateMessage &&
         this.sender.username !== message.sender &&
-        this.receiver.username !== message.sender
-      ) {
+        this.receiver.username !== message.sender)
+    );
+  }
+
+  shouldShowNewRoomMessageNotification(message) {
+    const isPrivateMessage = !!message.receiver;
+
+    return this.receiver && !isPrivateMessage;
+  }
+
+  ioAddMessage(message) {
+    if (this.sender.room === message.room) {
+      if (this.shouldShowNewPrivateMessageNotification(message)) {
         pubsub.pub('message:new-private', message.sender);
+      } else if (this.shouldShowNewRoomMessageNotification(message)) {
+        pubsub.pub('message:new', message.room);
       }
     }
 
-    if (
-      ((!this.receiver && isPrivate) ||
-        (this.receiver && this.receiver.username !== message.sender) ||
-        (this.receiver && !isPrivate)) &&
-      this.sender.username !== message.sender
-    ) {
+    if (this.isNotEligibleMessageForDisplay(message)) {
       return;
     }
 
-    io.emit('message:read', message.id);
-    message.read = true;
-    this.messages.push(new Message(message));
-
-    this.render();
+    pubsub.pub('message:add', message);
   }
 
-  ioEditMessage(message) {
-    const findMessage = this.messages.find(msg => msg.id === message.id);
-
-    if (!findMessage) {
-      return;
-    }
-
-    findMessage.text = message.text;
-    findMessage.edited = true;
-    findMessage.hasChanges = true;
-    this.render();
+  async ioMemberJoin(roomId) {
+    const messages = await this.loadMessages(roomId);
+    pubsub.pub('message:load', messages);
   }
 
-  ioDeleteMessage(id) {
-    const message = this.messages.find(msg => msg.id === id);
+  async loadMessages(roomId) {
+    let messages = await getRoomMessages(roomId);
 
-    if (!message) {
-      return;
-    }
+    messages = messages.map(message => ({
+      ...message,
+      read: new Date(message.timestamp).getTime() <= new Date(this.lastVisit).getTime(),
+    }));
 
-    this.elem.removeChild(message.elem);
-    const idx = this.messages.findIndex(msg => msg.id === id);
-    this.messages = this.messages.filter(msg => msg.id !== id);
-    if (this.messages[idx]) {
-      this.messages[idx].hasChanges = true;
-      this.render();
-    }
-  }
-
-  async loadMessages(room) {
-    const messages = await getRoomMessages(room.id);
-
-    this.messages = messages.map(
-      message =>
-        new Message({
-          ...message,
-          read: new Date(message.timestamp).getTime() <= new Date(this.lastVisit).getTime(),
-        }),
-    );
-    this.render(true);
-  }
-
-  joinRoom(room, lastVisit) {
-    this.lastVisit = lastVisit;
-  }
-
-  editMessage() {
-    this.render();
-  }
-
-  cancelEditMessage() {
-    this.render();
-  }
-
-  saveMessage(message, hasChanges) {
-    if (hasChanges) {
-      io.emit('message:edit', message);
-    }
-    this.render();
-  }
-
-  editLastMessage() {
-    const message = this.messages
-      .sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime())
-      .find(msg => msg.sender === this.sender.username && !msg.automated);
-
-    if (!message) {
-      return;
-    }
-
-    message.editing = true;
-    message.hasChanges = true;
-    this.render();
-  }
-
-  deleteMessage(message) {
-    this.elem.removeChild(message.elem);
-    const idx = this.messages.findIndex(msg => msg.id === message.id);
-    this.messages = this.messages.filter(msg => msg.id !== message.id);
-
-    if (this.messages[idx]) {
-      this.messages[idx].hasChanges = true;
-      this.render();
-    }
-
-    io.emit('message:delete', message.id);
+    return messages;
   }
 
   async selectMember(member) {
-    const messages = await getPrivateMessages(this.sender.room, member.id);
+    let messages = await getPrivateMessages(this.sender.room, member.id);
 
-    this.messages = messages.map(
-      message =>
-        new Message({
-          ...message,
-          read: new Date(message.timestamp).getTime() <= new Date(member.lastVisit).getTime(),
-        }),
-    );
+    messages = messages.map(message => ({
+      ...message,
+      read: new Date(message.timestamp).getTime() <= new Date(member.lastVisit).getTime(),
+    }));
     this.receiver = member;
-    this.render(true);
+    pubsub.pub('message:load', messages);
   }
 
-  selectRoom(room, lastVisit) {
+  async handleSelectRoom() {
     this.receiver = null;
+    pubsub.pub('message:load', []);
+  }
+
+  async handleJoinRoom(room, lastVisit) {
+    const messages = await this.loadMessages(room.id);
+
     this.lastVisit = lastVisit;
+    pubsub.pub('message:load', messages);
   }
 
-  addMessage(text) {
-    io.emit('message:add', text, this.sender, this.receiver);
+  handleWriteMessage(text) {
+    io.emit('message:add', text, this.receiver);
   }
 
-  scroll(clear) {
-    const parentElem = this.elem.parentElement;
-    const lastMessage = this.messages[this.messages.length - 1];
+  add(component) {
+    this.components.add(component);
 
-    if (!lastMessage) {
-      return;
-    }
-
-    const lastElem = lastMessage.elem;
-    const height = parentElem.scrollTop + parentElem.clientHeight + lastElem.clientHeight;
-
-    if (
-      height === parentElem.scrollHeight ||
-      parentElem.scrollTop === 0 ||
-      lastMessage.sender === this.sender.username ||
-      clear
-    ) {
-      parentElem.scrollTop = parentElem.scrollHeight;
-    }
+    return this;
   }
 
-  render(clear = false) {
-    const messageOutput = new MessageOutput();
-    const messageRepeatedOutput = new MessageRepeatedOutput();
-    const messageEditOutput = new MessageEditOutput();
-    const messageNewOutput = new MessageNewOutput();
+  remove(component) {
+    this.components.delete(component);
 
-    let newElem;
-    let prevSender;
-    let isReaded = true;
-    let isSetNewMessagesLabel = false;
+    return this;
+  }
 
-    if (clear) {
-      this.elem.innerHTML = '';
-    }
-
-    this.messages
-      .sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime())
-      .forEach(message => {
-        if (message.rendered && !message.hasChanges) {
-          prevSender = message.sender;
-          return;
-        }
-
-        if (message.read !== isReaded && !message.automated && !isSetNewMessagesLabel) {
-          newElem = message.render(messageNewOutput);
-          this.elem.appendChild(newElem);
-          isSetNewMessagesLabel = true;
-        }
-
-        if (message.editing) {
-          newElem = message.render(messageEditOutput);
-        } else if (message.sender !== prevSender) {
-          newElem = message.render(messageOutput);
-        } else {
-          newElem = message.render(messageRepeatedOutput);
-        }
-
-        if (!message.hasChanges) {
-          this.elem.appendChild(newElem);
-        } else {
-          message.elem.replaceWith(newElem);
-        }
-
-        prevSender = message.sender;
-        isReaded = message.read;
-        message.rendered = true;
-        message.elem = newElem;
-      });
-
-    this.scroll(clear);
+  render() {
+    this.components.forEach(component => {
+      component.render();
+    });
   }
 }
 
