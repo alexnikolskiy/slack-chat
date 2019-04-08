dac31e33e18eeb67131f191d3bfc4c0b6535ef68
Alexander Nikolskiy <alexxxnikolskiy@gmail.com>
2019-04-08 21:03:07 +0300

    Protect api route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { protectApi } = require('../middlewares/auth');
 
 const router = express.Router();
 
 const roomsRouter = require('./api/rooms');
 const usersRouter = require('./api/users');
 
+router.use(protectApi);
 router.use('/rooms', roomsRouter);
 router.use('/users', usersRouter);
 
