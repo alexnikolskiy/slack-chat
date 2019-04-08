b8d69b91be6ed4c0c01f57402dfe8d42429a6ae4
Alexander Nikolskiy <alexxxnikolskiy@gmail.com>
2019-04-08 18:34:18 +0300

    Modify NotificationBar component

--- src/js/components/NotificationBar.js
+++ src/js/components/NotificationBar.js
@@ -1,19 +1,21 @@
 import template from 'Templates/notification-bar';
-import io from 'Utils/io';
-import pubsub from 'Utils/pubsub';
+import Component from '../lib/component';
+import store from '../store';
 
-class NotificationBar {
-  constructor() {
-    this.elem = document.querySelector('.notification-bar');
-    this.text = '';
-    this.sender = null;
-    this.receiver = null;
+class NotificationBar extends Component {
+  constructor(io, pubsub) {
+    super({
+      store,
+      elem: document.querySelector('.notification-bar'),
+    });
 
-    pubsub.sub('member:select', this.handleSelectMember.bind(this));
-    pubsub.sub('room:select', this.handleSelectRoom.bind(this));
+    this.state = {
+      text: '',
+    };
+    this.io = io;
+    this.pubsub = pubsub;
 
-    io.on('editor:typing', this.ioMessageTyping.bind(this));
-    io.on('login', this.ioLogin.bind(this));
+    this.io.on('editor:typing', this.ioMessageTyping.bind(this));
   }
 
   static getTypingText(users) {
@@ -31,36 +33,26 @@ class NotificationBar {
     return text;
   }
 
-  ioMessageTyping(users, sender, receiver = null) {
-    const isPrivateChatting = !!this.receiver;
-    let userList = users.filter(user => user !== this.sender.username);
+  ioMessageTyping(users, isPrivateMessage) {
+    const { sender, receiver } = store.getState();
+    const isPrivateChatting = !!receiver;
+    let userList = users.filter(user => user !== sender.username);
 
     if (
-      (!isPrivateChatting && receiver) ||
-      (isPrivateChatting && !receiver) ||
-      (isPrivateChatting && this.receiver.username === this.sender.username)
+      (isPrivateMessage && !isPrivateChatting) ||
+      (!isPrivateMessage && isPrivateChatting) ||
+      (isPrivateMessage && isPrivateChatting && receiver.username === sender.username)
     ) {
       userList = [];
     }
 
-    this.text = NotificationBar.getTypingText(userList);
-    this.render();
-  }
-
-  handleSelectMember(member) {
-    this.receiver = member;
-  }
-
-  handleSelectRoom() {
-    this.receiver = null;
-  }
-
-  ioLogin(user) {
-    this.sender = user;
+    this.setState({
+      text: NotificationBar.getTypingText(userList),
+    });
   }
 
   render() {
-    this.elem.innerHTML = template({ text: this.text });
+    this.elem.innerHTML = template({ text: this.state.text });
   }
 }
 
